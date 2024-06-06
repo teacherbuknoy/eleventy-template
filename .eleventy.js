697637d3ca74dbf5cdc39db31b2f6b11d664c290
Francis Rubio <francis.rubio@dice205.com>
2024-06-06 17:29:42 +0800

    Added blog layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,13 @@
 const collections = require('./src/config/collections.js')
+const filters = require('./src/config/filters.js') 
 
 module.exports = function (config) {
   Object.keys(collections).forEach(collectionType => {
     config.addCollection(collectionType, collections[collectionType])
+  })
+
+  Object.keys(filters).forEach(name => {
+    config.addFilter(name, filters[name])
   })
 
   return {
@@ -10,7 +15,8 @@ module.exports = function (config) {
       input: 'src',
       output: 'public',
       layouts: 'templates',
-      includes: 'components'
+      includes: 'components',
+      data: 'data'
     },
     templateFormats: ['md', 'html', 'njk']
   }
